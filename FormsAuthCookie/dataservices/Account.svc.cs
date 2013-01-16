3b88125497249b1bbd8a9d773617ad5ac90a63dd
WesDaniels <github@wesdaniels.com>
2013-01-16 10:23:21 -0600

    Service Initial Build
    Signed-off-by: WesDaniels <github@wesdaniels.com>

---
+++ FormsAuthCookie/dataservices/Account.svc.cs
@@ -0,0 +1,26 @@
+﻿namespace FormsAuthCookie.dataservices
+{
+  using System;
+  using System.ServiceModel.Activation;
+  using System.Web;
+
+  [AspNetCompatibilityRequirements(RequirementsMode = AspNetCompatibilityRequirementsMode.Allowed)]
+  public class Account : IAccount
+  {
+    public string Ping()
+    {
+      return "Pong!";
+    }
+
+    public string Login(string name)
+    {
+      HttpCookie myCookie = new HttpCookie("USCookie");
+      myCookie.HttpOnly = true;
+      myCookie["UniqueId"] = "I'm Unique!";
+      myCookie.Expires = DateTime.Now.AddDays(1d);
+      HttpContext.Current.Response.Cookies.Add(myCookie);
+
+      return name;
+    }
+  }
+}
---
+++ FormsAuthCookie/dataservices/secure/Account.svc.cs
@@ -0,0 +1,24 @@
+﻿namespace FormsAuthCookie.dataservices.secure
+{
+  using System.ServiceModel.Activation;
+  using System.Web;
+
+  [AspNetCompatibilityRequirements(RequirementsMode = AspNetCompatibilityRequirementsMode.Allowed)]
+  public class Account : IAccount
+  {
+    public string Ping()
+    {
+      return "Pong!";
+    }
+
+    public string GetCustomer()
+    {
+      var httpCookie = HttpContext.Current.Request.Cookies["USCookie"];
+      if (httpCookie != null && httpCookie["UniqueId"] != null)
+      {
+        return httpCookie["UniqueId"];
+      }
+      return "not found";
+    }
+  }
+}
