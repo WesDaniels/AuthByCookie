3ae1d3a493bbf4e0fdd94089b8508a680fac4b2b
WesDaniels <github@wesdaniels.com>
2013-01-16 16:50:10 -0600

    Include a UI for Demo presentation
    Signed-off-by: WesDaniels <github@wesdaniels.com>

---
+++ dataservices/secure/IAccount.cs
@@ -0,0 +1,27 @@
+﻿namespace FormsAuthCookie.dataservices.secure
+{
+  using System.ServiceModel;
+  using System.ServiceModel.Web;
+
+  [ServiceContract]
+  public interface IAccount
+  {
+    [WebInvoke(Method = "GET",
+            ResponseFormat = WebMessageFormat.Json,
+            BodyStyle = WebMessageBodyStyle.Wrapped,
+            UriTemplate = "Ping")]
+    string Ping();
+
+    [WebInvoke(Method = "GET",
+        ResponseFormat = WebMessageFormat.Json,
+        BodyStyle = WebMessageBodyStyle.Wrapped,
+        UriTemplate = "getcustomer")]
+    string GetCustomer();
+
+    [WebInvoke(Method = "GET",
+        ResponseFormat = WebMessageFormat.Json,
+        BodyStyle = WebMessageBodyStyle.Wrapped,
+        UriTemplate = "logout")]
+    void LogOut();
+  }
+}
